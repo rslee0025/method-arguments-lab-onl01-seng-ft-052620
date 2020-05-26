f4f9b773b606a4607224713194dc211dd86211c1
Rachel S. Lee <rslee0025@gmail.com>
2020-05-26 20:34:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name)
-  puts "Hi,my name is #{name}."
+  puts "Hi, my name is #{name}."
 end  
 
 introduction("Josh")
@@ -8,4 +8,4 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
 
-introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Dan", "Python")
