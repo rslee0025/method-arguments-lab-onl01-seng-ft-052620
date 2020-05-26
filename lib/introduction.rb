a745c4838211521e04ed098179029e406140e395
Rachel S. Lee <rslee0025@gmail.com>
2020-05-26 20:38:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,5 +8,5 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end  
 
-introduction_with_language("Rachel", "Python")
+introduction_with_language("Rachel", "Ruby")
 
